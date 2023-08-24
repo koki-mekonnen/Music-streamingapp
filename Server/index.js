608f2d9756a772af14f48628bd38d29da970204a
selamawit mekonnen <ezanawittadele@gmail.com>
2023-08-24 10:56:27 +0300

    first commit

---
+++ Server/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+require("dotenv").config();
+const app = express();
+
+
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+    console.log(`server started working on port  ${PORT}`)
+})
+
